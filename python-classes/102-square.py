0436ae258ee5e03055e807f3b86ce7429df1e5e8
Kanisa1 <k.thiak@aludstudent.com>
2023-11-11 15:48:52 +0200

    102-square.py

--- python-classes/102-square.py
+++ python-classes/102-square.py
@@ -1,27 +1,42 @@
 #!/usr/bin/python3
-"""creates class Square with
-private instance attribute size and public instance method"""
-
-
 class Square:
-    """defines class with instantiated and validated private instance attribute
-and public instance method."""
-
+    """Defines a square"""
     def __init__(self, size=0):
-        self.__size = size
+        """Initialises the data"""
+        self.size = size
+
+    def area(self):
+        """Returns current square area"""
+        return self.__size**2
 
     @property
     def size(self):
-        return(self.__size)
+        """Getter method"""
+        return self.__size
 
     @size.setter
     def size(self, value):
-        if type(value) is not int:
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
+        """Setter method"""
         self.__size = value
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
 
-    def area(self):
-        """calculates and returns current square area"""
-        return(self.__size * self.__size)
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
