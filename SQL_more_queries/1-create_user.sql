176cb0b780b1102081315eca1237d382721ef724
Kanisa1 <k.thiak@alustudent.com>
2023-11-28 03:09:17 -0800

    1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,7 +1,2 @@
--- Creates user
--- create user
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
--- grant privs
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
--- flush
-FLUSH PRIVILEGES;
+-- Creates the user user_0d_1
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
