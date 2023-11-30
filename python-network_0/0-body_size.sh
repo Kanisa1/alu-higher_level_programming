9eda43bfd12796bfaeaff8bd06995f40ed2c060c
Kanisa1 <k.thiak@alustudent.com>
2023-11-30 20:21:18 +0200

    .\0-body_size.sh

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# script to get the body size of a request
-curl -Is "$1" | grep -w 'Content-Length' | cut -f2 -d' '
-
+# displays size of the body of the response
+curl -sI "$1" | grep 'Content-Length' | awk '{print $2}'
