d4357079a68a0619ad424d01f9dffd5cb6c594d1
Kanisa1 <k.thiak@alustudent.com>
2023-12-06 14:46:02 +0200

    .\3-say_my_name.py

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -14,4 +14,5 @@ def say_my_name(first_name, last_name=""):
         raise TypeError('last_name must be a string')
     else:
         print("My name is {} {}". format(first_name, last_name))
+
     
