915cc0e34226a11ad174ca01a9898fb92bbc21fd
Kanisa1 <k.thiak@alustudent.com>
2023-12-17 03:51:46 -0800

    11-model_state_insert.py

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
-'''a script that adds the State object
-“Louisiana” to the database hbtn_0e_6_usa'''
+"""
+This script adds the State object
+`Louisiana` to the database `hbtn_0e_6_usa`.
+"""
 
-
-import sys
+from sys import argv
 from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    Base.metadata.create_all(engine)
+    """
+    Access to the database and get a state
+    from the database.
+    """
 
-    state = State(name="Louisiana")
-    session.add(state)
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(db_uri)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    lou_state = State(name='Louisiana')
+    session.add(lou_state)
     session.commit()
-    print(state.id)
+    print('{0}'.format(lou_state.id))
     session.close()
