9f03570a9846f24689fd18bcd8d649c250f6a1f5
Kanisa1 <k.thiak@alustudent.com>
2023-12-17 03:29:51 -0800

    5-filter_cities.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,36 +1,41 @@
 #!/usr/bin/python3
-'''a script that takes in the name of a state as an argument and
-lists all cities of that state, using the database hbtn_0e_4_usa'''
+"""
+This script  takes in the name of a state
+as an argument and lists all cities of that
+state, using the database `hbtn_0e_4_usa`.
+"""
 
-
-import sys
 import MySQLdb
+from sys import argv
 
+if __name__ == '__main__':
+    """
+    Access to the database and get the cities
+    from the database.
+    """
 
-if __name__ == "__main__":
-    conn = MySQLdb.connect(
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        host="localhost",
-        port=3306
-    )
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
-    state_name = sys.argv[4]
+    with db.cursor() as cur:
+        cur.execute("""
+            SELECT
+                cities.id, cities.name
+            FROM
+                cities
+            JOIN
+                states
+            ON
+                cities.state_id = states.id
+            WHERE
+                states.name LIKE BINARY %(state_name)s
+                 ORDER BY
+                cities.id ASC
+        """, {
+            'state_name': argv[4]
+        })
 
-    cursor = conn.cursor()
+        rows = cur.fetchall()
 
-    sql_com = """SELECT cities.name
-          FROM states
-          INNER JOIN cities ON states.id = cities.state_id
-          WHERE states.name = %s
-          ORDER BY cities.id ASC"""
-
-    cursor.execute(sql_com, (state_name,))
-
-    data = cursor.fetchall()
-
-    print(", ".join([city[0] for city in data]))
-
-    cursor.close()
-    conn.close()
+    if rows is not None:
+        print(", ".join([row[1] for row in rows]))
