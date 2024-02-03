0de05c00be8d21739add4840c6cd9fd3fbaa0dd9
Kanisa1 <k.thiak@alustudent.com>
2024-02-03 20:52:37 +0000

    6-multi_languages_loop.js

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-let i = 0;
-while (i < 3) {
-  console.log(array[i]);
-  i++;
+const line = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < line.length; i++) {
+  console.log(line[i]);
 }
